826841eef76f3a2afa8b6860b03128d4d70013a9
unknown <Marcelo>
2018-05-16 22:38:31 -0500

    trabaja bibobab

--- ProyectoBuques/src/proyecto/buque/Buque.java
+++ ProyectoBuques/src/proyecto/buque/Buque.java
@@ -1,16 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package proyecto.buque;
 
 import proyecto.contenedor.Contenedor;
 
-/**
- *
- * @author v6216
- */
+
 public class Buque {
     private Contenedor[] contenedores = new Contenedor[50];
     private int numeroContenedores;
--- ProyectoBuques/src/proyecto/prueba/Prueba.java
+++ ProyectoBuques/src/proyecto/prueba/Prueba.java
@@ -1,17 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package proyecto.prueba;
 
 import proyecto.buque.Buque;
 import proyecto.contenedor.Contenedor;
 
-/**
- *
- * @author v6216
- */
+
 public class Prueba {
     
     public static void main(String[] args) {
