4084f1b50dd02fed4f3c217525e11652e4000e11
Nicolas <nmoran@nicolasservices.com.pe>
2018-07-09 21:55:07 -0500

    Marcelo gay

--- Trabajo2-Inocente-Moran/src/miVentana/MiFormulario.java
+++ Trabajo2-Inocente-Moran/src/miVentana/MiFormulario.java
@@ -48,12 +48,13 @@ public class MiFormulario extends javax.swing.JFrame {
         jScrollPane2 = new javax.swing.JScrollPane();
         userTable = new javax.swing.JTable();
         seeUserTable = new javax.swing.JButton();
-        seeMessages = new javax.swing.JButton();
+        seeRandom = new javax.swing.JButton();
         userLoginField = new javax.swing.JTextField();
         jLabel1 = new javax.swing.JLabel();
         jLabel7 = new javax.swing.JLabel();
         passwordLoginField = new javax.swing.JTextField();
         exitButton = new javax.swing.JButton();
+        seeMessages = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Gestión de un Servidor de Correo");
@@ -247,12 +248,12 @@ public class MiFormulario extends javax.swing.JFrame {
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
-        seeMessages.setFont(new java.awt.Font("Comic Sans MS", 0, 14)); // NOI18N
-        seeMessages.setText("Ver Mensaje de su Buzón");
-        seeMessages.setActionCommand("   SALIR");
-        seeMessages.addActionListener(new java.awt.event.ActionListener() {
+        seeRandom.setFont(new java.awt.Font("Comic Sans MS", 0, 14)); // NOI18N
+        seeRandom.setText("Ver Mensaje Aleatorio");
+        seeRandom.setActionCommand("   SALIR");
+        seeRandom.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                seeMessagesActionPerformed(evt);
+                seeRandomActionPerformed(evt);
             }
         });
 
@@ -267,6 +268,15 @@ public class MiFormulario extends javax.swing.JFrame {
             }
         });
 
+        seeMessages.setFont(new java.awt.Font("Comic Sans MS", 0, 14)); // NOI18N
+        seeMessages.setText("Ver Mensaje de su Buzón");
+        seeMessages.setActionCommand("   SALIR");
+        seeMessages.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                seeMessagesActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
@@ -276,30 +286,30 @@ public class MiFormulario extends javax.swing.JFrame {
                 .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addGap(27, 27, 27)
-                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addComponent(userLoginField, javax.swing.GroupLayout.PREFERRED_SIZE, 142, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                    .addComponent(jLabel1))
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addComponent(jLabel7)
-                                    .addComponent(passwordLoginField, javax.swing.GroupLayout.PREFERRED_SIZE, 157, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                            .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addGroup(jPanel1Layout.createSequentialGroup()
-                                        .addGap(43, 43, 43)
-                                        .addComponent(exitButton, javax.swing.GroupLayout.PREFERRED_SIZE, 75, javax.swing.GroupLayout.PREFERRED_SIZE))
-                                    .addGroup(jPanel1Layout.createSequentialGroup()
-                                        .addGap(35, 35, 35)
-                                        .addComponent(seeMessages, javax.swing.GroupLayout.PREFERRED_SIZE, 223, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                                .addGap(0, 0, Short.MAX_VALUE)))
-                        .addGap(28, 28, 28))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGap(18, 18, 18)
                         .addComponent(jPanel4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addContainerGap(20, Short.MAX_VALUE))))
+                        .addContainerGap(20, Short.MAX_VALUE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(27, 27, 27)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addComponent(seeMessages, javax.swing.GroupLayout.PREFERRED_SIZE, 223, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                                    .addGroup(jPanel1Layout.createSequentialGroup()
+                                        .addComponent(seeRandom)
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                        .addComponent(exitButton, javax.swing.GroupLayout.PREFERRED_SIZE, 75, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                    .addGroup(jPanel1Layout.createSequentialGroup()
+                                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                            .addComponent(userLoginField, javax.swing.GroupLayout.PREFERRED_SIZE, 142, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                            .addComponent(jLabel1))
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                            .addComponent(jLabel7)
+                                            .addComponent(passwordLoginField, javax.swing.GroupLayout.PREFERRED_SIZE, 157, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                                .addGap(28, 28, 28))))))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -318,10 +328,13 @@ public class MiFormulario extends javax.swing.JFrame {
                             .addComponent(userLoginField, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(passwordLoginField, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE))
                         .addGap(18, 18, 18)
-                        .addComponent(seeMessages, javax.swing.GroupLayout.PREFERRED_SIZE, 39, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(18, 18, 18)
-                        .addComponent(exitButton, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap(23, Short.MAX_VALUE))
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addComponent(seeMessages, javax.swing.GroupLayout.PREFERRED_SIZE, 39, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addComponent(seeRandom, javax.swing.GroupLayout.PREFERRED_SIZE, 39, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addComponent(exitButton, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                .addContainerGap(29, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -387,13 +400,25 @@ public class MiFormulario extends javax.swing.JFrame {
         userTable.setModel(modelo);
     }//GEN-LAST:event_seeUserTableActionPerformed
 
+    private void seeRandomActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_seeRandomActionPerformed
+        getMessage(false);
+    }//GEN-LAST:event_seeRandomActionPerformed
+
+    private void exitButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_exitButtonActionPerformed
+        System.exit(0);
+    }//GEN-LAST:event_exitButtonActionPerformed
+
     private void seeMessagesActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_seeMessagesActionPerformed
+        getMessage(true);
+    }//GEN-LAST:event_seeMessagesActionPerformed
+
+    private void getMessage(boolean first) {
         if (fieldIsValid(userLoginField, passwordLoginField)) {
             NodoLEG<Usuario> ptr = userManager.getNodo();
             boolean loggedIn = false;
             while (ptr != null) {
                 if (ptr.getInfo().getNombre().equals(userLoginField.getText()) && ptr.getInfo().getPassword().equals(passwordLoginField.getText())) {
-                    Mensaje mensaje = ptr.getInfo().eliminarDeCola(true);
+                    Mensaje mensaje = ptr.getInfo().eliminarDeCola(first);
                     JOptionPane.showMessageDialog(null, mensaje != null ? mensaje.toString() : "¡No hay mensajes!");
                     loggedIn = true;
                 }
@@ -403,12 +428,8 @@ public class MiFormulario extends javax.swing.JFrame {
                 JOptionPane.showMessageDialog(null, "Usuario incorrecto");
             }
         }
-    }//GEN-LAST:event_seeMessagesActionPerformed
-
-    private void exitButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_exitButtonActionPerformed
-        System.exit(0);
-    }//GEN-LAST:event_exitButtonActionPerformed
-
+    }
+    
     private boolean fieldIsValid(javax.swing.JTextField... fields) {
         boolean isValid = true;
         for (javax.swing.JTextField field : fields) {
@@ -478,6 +499,7 @@ public class MiFormulario extends javax.swing.JFrame {
     private javax.swing.JTextField passwordField;
     private javax.swing.JTextField passwordLoginField;
     private javax.swing.JButton seeMessages;
+    private javax.swing.JButton seeRandom;
     private javax.swing.JButton seeUserTable;
     private javax.swing.JButton sendMessage;
     private javax.swing.JTextField senderUser;
